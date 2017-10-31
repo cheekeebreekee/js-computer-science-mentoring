91a2feac86a9577b78e6838b66141d5acb70644c
Kashif Minhaj <dailypctips@gmail.com>
2017-10-31 19:45:21 +0530

    Implement simple stack data structure

--- stack/stack.js
+++ stack/stack.js
@@ -1,20 +1,35 @@
 import Node from './node';
 
 export default class Stack {
+
   constructor() {
-    this.top = null;
+    this._top = null;
     this.length = 0;
+    this.items = [];
   }
 
   pop() {
     // delete the top element of stack
+    if(this._length == 0) {
+      return null;
+    } else {
+      let poppedNode = this.items.pop();
+      this.length -= 1;
+      this._top = this.items[this.length - 1] || null;
+      return poppedNode;
+    }
   }
 
   push(value) {
     // add element on top of stack
+    let node = new Node(value);
+    this.items.push(node);
+    this._top = node;
+    this.length += 1;
   }
 
   peek() {
     // returns value of top stack element
+    return top;
   }
 }
